82a2f15884ce28cc629b50816129097ed85027d8
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2024-01-20 14:47:21 +0800

    feat: add popup

--- client/CodCaptcha/TakeOverView.swift
+++ client/CodCaptcha/TakeOverView.swift
@@ -8,9 +8,72 @@
 import SwiftUI
 
 struct TakeOverView: View {
+    
+    @State private var showBackgroundBlur = false
+    @State private var showPopupSheet = false
+    
+    @State private var fishOffsetX = 1000.0
+    @State private var fishOffsetY = 500.0
+    
     var body: some View {
-        Rectangle()
-            .fill(.thinMaterial)
+        ZStack {
+            if showBackgroundBlur {
+                Rectangle()
+                    .fill(.ultraThinMaterial)
+            }
+            
+            Button("kill") {
+                NSApplication.shared.terminate(nil)
+            }
+            .frame(maxHeight: .infinity, alignment: .bottom)
+            
+            ZStack {
+                if showPopupSheet {
+                    VStack(alignment: .leading) {
+                        Text("Are you a robot?")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        Text("For every megabyte of data sent or received, you will have to solve a CAPTCHA.")
+                            .font(.body)
+                        
+                        Divider()
+                            .padding(.vertical)
+                        
+                        Text("1 of 30")
+                        
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                    .background(.background)
+                    .clipShape(.rect(cornerRadius: 16))
+                }
+                
+                Image(.fish)
+                    .resizable()
+                    .scaledToFit()
+                    .rotationEffect(.degrees(20))
+                    .frame(width: 150)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                    .offset(x: fishOffsetX, y: fishOffsetY)
+                
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .frame(width: 600, height: 600)
+        }
+        .onAppear {
+            withAnimation {
+                showPopupSheet = true
+            }
+            withAnimation(.easeOut(duration: 1)) {
+                fishOffsetX = 100
+                fishOffsetY = 0
+            } completion: {
+                withAnimation(.easeInOut(duration: 1)) {
+                    showBackgroundBlur = true
+                }
+            }
+        }
     }
 }
 
--- client/CodCaptcha/ViewController.swift
+++ client/CodCaptcha/ViewController.swift
@@ -40,25 +40,29 @@ class ViewController: NSViewController {
         window.hasShadow = false
         window.styleMask = [.borderless]
         window.collectionBehavior = [.canJoinAllSpaces, .stationary, .fullScreenAuxiliary]
-        window.backgroundColor = .red.withAlphaComponent(0.1)
+        window.backgroundColor = .clear
         window.titlebarAppearsTransparent = true
         
         window.title = ""
         
         window.toolbar = nil
-        window.ignoresMouseEvents = true
-        window.acceptsMouseMovedEvents = false
+        window.ignoresMouseEvents = false
         
         window.isMovableByWindowBackground = false
         
-        window.setFrame(NSScreen.main!.frame, display: true)
+        window.setFrame(.zero, display: true)
         window.isMovable = false
         window.titleVisibility = .hidden
         window.makeKeyAndOrderFront(nil)
         
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { _ in
             self.createPopover()
         }
+    }
+    
+    override func viewDidLayout() {
+        super.viewDidLayout()
+        
     }
     
     func createPopover() {
@@ -80,5 +84,7 @@ class ViewController: NSViewController {
         popover = newView
         
         view.addSubview(popover)
+        
+        view.window!.setFrame(NSScreen.main!.frame, display: true)
     }
 }
